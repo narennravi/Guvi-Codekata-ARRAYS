46a6d6bcff119901f2b19d690c66422b5ba3a2c6
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-06-24 22:20:55 +0530

    You are given with an array. For each element present in the array your task is to print the next smallest than that number.  If it is not smallest print -1

---
+++ 07.py
@@ -0,0 +1,37 @@
+# Question:-
+'''
+You are given with an array. For each element present in the array your task is to print the next smallest than that number. 
+If it is not smallest print -1
+
+Input Description:
+You are given a number ‘n’ representing size of array. And n space separated numbers.
+
+Output Description:
+Print the next smallest number present in array and -1 if no smallest is present
+
+
+Sample Input :
+7
+10 7 9 3 2 1 15
+
+Sample Output :
+7 3 3 2 1 -1 -1
+'''
+
+
+
+# Solution:-
+n=int(input())
+arr=list(map(int,input().split()))
+c=[]
+for i in range(n):
+  value=0
+  for j in range(i,n):
+    if arr[i]>arr[j]:
+      value=arr[j]
+      break
+    else:
+      value=-1
+
+  c.append(value)
+print(*c)
