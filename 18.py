bfa79202cad1dc5791a3364e516ff21a38a5ed58
Narendran R <94606963+narennravi@users.noreply.github.com>
2023-06-27 12:21:11 +0530

    Given a string S, print it without using semicolon in your program.

---
+++ 18.py
@@ -0,0 +1,16 @@
+# Question:-
+'''
+Given a string S, print it without using semicolon in your program.
+
+Sample Testcase :
+INPUT
+hello world
+
+OUTPUT
+hello world
+'''
+
+
+# Solution:-
+S=str(input())
+print(S)
